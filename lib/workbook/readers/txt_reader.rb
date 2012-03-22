0424f6c06704a6c5a5f60d5eb7bcc5a0f0541537
Maarten Brouwers <github.com@murb.nl>
2012-03-22 15:52:02 +0100

    windows compatibility

--- lib/workbook/readers/txt_reader.rb
+++ lib/workbook/readers/txt_reader.rb
@@ -9,7 +9,7 @@ module Workbook
       
       def parse_txt csv_raw
         csv = []
-        csv_raw.split("\r\n").each {|l| csv << FasterCSV.parse_line(l,{:col_sep=>"\t"});nil}
+        csv_raw.split("\n").each {|l| csv << FasterCSV.parse_line(l,{:col_sep=>"\t"});nil}
         self[0]=Workbook::Sheet.new(csv,self,{:parse_cells_on_batch_creation=>true, :cell_parse_options=>{:detect_date=>true}}) unless sheet.has_contents?
       end
     end
