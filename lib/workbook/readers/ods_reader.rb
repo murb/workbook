57c5887f13693ab0e9bc20b5bfd8c8e67764d76c
Maarten Brouwers <github.com@murb.nl>
2013-04-02 13:41:57 +0200

    preparing for ods import ... but rods isn't to my liking...

---
+++ lib/workbook/readers/ods_reader.rb
@@ -0,0 +1,57 @@
+# -*- encoding : utf-8 -*-
+require 'rods'
+# TODO: rods works weird... write my own parser.
+
+module Workbook
+  module Readers
+    module OdsReader
+      def load_ods file_obj
+        file_obj = file_obj.path if file_obj.is_a? File
+        sp = Rods.new(file_obj)
+        template.add_raw sp
+        parse_ods sp
+      end
+      
+      def parse_ods_cell cell
+        value, type = cell.read_cell
+        if type == "string"
+          return value
+        elsif type == "float"
+          if value.to_f - value.to_i == 0.0
+            value = value.to_i
+          else
+            value = value.to_f
+          end
+        elsif type == "date"
+          year,month,day= cell.attributes["office:date-value"].split('-')
+          value = Time.new(year.to_i,month.to_i,day.to_i)
+        end
+        return Workbook::Cell.new(value)
+      end
+      
+      def parse_ods_row sheet, row_index
+        values = Workbook::Row.new
+        cell=sheet.getCell(row_index,1)
+        values.push parse_ods_cell(cell)
+        while(cell=mySheet.getNextExistentCell(cell)) do
+          values.push parse_ods_cell(cell)
+        end
+        return values
+      end
+      
+      def parse_ods ods_spreadsheet=template.raws[Rods], options={}
+        options = {:additional_type_parsing=>false}.merge options
+        row_index = 1
+        row=ods_spreadsheet.getRow(1)
+        wb = Workbook::Book.new
+        table = wb.sheet.first
+        table.push(parse_ods_row(ods_spreadsheet,row))
+        while(row=mySheet.getNextExistentRow(row))
+          row_index += 1
+          table.push(parse_ods_row(ods_spreadsheet,row_index))
+        end
+        return wb
+      end
+    end
+  end
+end
---
+++ test/test_readers_ods_reader.rb
@@ -0,0 +1,46 @@
+# -*- encoding : utf-8 -*-
+require File.join(File.dirname(__FILE__), 'helper')
+module Readers
+  class TestXlsWriter < Test::Unit::TestCase
+    def test_open
+      
+      w = Workbook::Book.new
+      #w.open 'test/artifacts/book_with_tabs_and_colours.ods'
+      # assert_equal([:a, :b, :c, :d, :e],w.sheet.table.header.to_symbols)
+ #      assert_equal(90588,w.sheet.table[2][:b].value)
+ #      assert_equal("#CCFFCC",w.sheet.table[3][:c].format[:background_color])
+ #      assert_equal(8.13671875,w.sheet.table.first[:b].format[:width])
+ #      assert_equal(3.85546875,w.sheet.table.first[:a].format[:width])
+ #      assert_equal(25.14453125,w.sheet.table.first[:c].format[:width])
+ #      
+ #      
+    end
+    
+    def test_complex_types
+      w = Workbook::Book.new
+     # w.open 'test/artifacts/complex_types.ods'
+      # assert_equal(Date.new(2011,11,15), w.sheet.table[2][3].value)
+ #      assert_equal("http://murb.nl", w.sheet.table[3][2].value)
+ #      assert_equal("sadfasdfsd", w.sheet.table[4][2].value)
+ #      assert_equal(1.2, w.sheet.table[3][1].value)
+    end
+    
+    def test_excel_standardized_open
+      w = Workbook::Book.new
+     # w.open("test/artifacts/excel_different_types.ods")
+      # reads
+      #   a,b,c,d
+      # 2012-02-22,2014-12-27,2012-11-23,2012-11-12T04:20:00+00:00
+      # c,222.0,,0027-12-14T05:21:00+00:00
+      # 2012-01-22T11:00:00+00:00,42000.0,"goh, idd",ls
+            # 
+      # assert_equal([:a,:b,:c, :d],w.sheet.table.header.to_symbols)
+      # assert_equal(Date.new(2012,2,22),w.sheet.table[1][:a].value)
+      # assert_equal("c",w.sheet.table[2][:a].value)
+      # assert_equal(DateTime.new(2012,1,22,11),w.sheet.table[3][:a].value)
+      # assert_equal(42000,w.sheet.table[3][:b].value)
+      # assert_equal(nil,w.sheet.table[2][:c].value)
+    end
+    
+  end
+end
