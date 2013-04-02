03114d3de2e59bf8ec90f73ae8cfbd4a85bd1a54
Maarten Brouwers <github.com@murb.nl>
2013-04-02 13:44:35 +0200

    removed rods require statement...

--- lib/workbook/readers/ods_reader.rb
+++ lib/workbook/readers/ods_reader.rb
@@ -1,6 +1,6 @@
 # -*- encoding : utf-8 -*-
-require 'rods'
-# TODO: rods works weird... write my own parser.
+
+# TODO: rods (require 'rods') works weird... going to write my own parser.
 
 module Workbook
   module Readers
