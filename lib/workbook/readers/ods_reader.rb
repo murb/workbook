fdbbede87ed97d5596db8206e2959601e7edc551
Maarten Brouwers <github.com@murb.nl>
2013-05-04 17:37:27 +0200

    basic ods parsing

--- lib/workbook/readers/ods_reader.rb
+++ lib/workbook/readers/ods_reader.rb
@@ -7,50 +7,44 @@ module Workbook
     module OdsReader
       def load_ods file_obj
         file_obj = file_obj.path if file_obj.is_a? File
-        sp = Rods.new(file_obj)
-        template.add_raw sp
-        parse_ods sp
-      end
-      
-      def parse_ods_cell cell
-        value, type = cell.read_cell
-        if type == "string"
-          return value
-        elsif type == "float"
-          if value.to_f - value.to_i == 0.0
-            value = value.to_i
-          else
-            value = value.to_f
+        content = ""
+        Zip::ZipFile.open(file_obj) do |zipfile|
+          zipfile.entries.each do |file|
+            content = zipfile.read(file.name) if file.name == "content.xml"  
           end
-        elsif type == "date"
-          year,month,day= cell.attributes["office:date-value"].split('-')
-          value = Time.new(year.to_i,month.to_i,day.to_i)
         end
-        return Workbook::Cell.new(value)
+        content = Nokogiri.XML(content)
+        template.add_raw content
+        parse_ods content
       end
       
-      def parse_ods_row sheet, row_index
-        values = Workbook::Row.new
-        cell=sheet.getCell(row_index,1)
-        values.push parse_ods_cell(cell)
-        while(cell=mySheet.getNextExistentCell(cell)) do
-          values.push parse_ods_cell(cell)
-        end
-        return values
-      end
-      
-      def parse_ods ods_spreadsheet=template.raws[Rods], options={}
+      def parse_ods ods_spreadsheet=template.raws[Nokogiri::XML::Document], options={}
         options = {:additional_type_parsing=>false}.merge options
-        row_index = 1
-        row=ods_spreadsheet.getRow(1)
-        wb = Workbook::Book.new
-        table = wb.sheet.first
-        table.push(parse_ods_row(ods_spreadsheet,row))
-        while(row=mySheet.getNextExistentRow(row))
-          row_index += 1
-          table.push(parse_ods_row(ods_spreadsheet,row_index))
+        ods_spreadsheet.xpath("//office:body/office:spreadsheet").each_with_index do |sheet,sheetindex|
+          s = self.create_or_open_sheet_at(sheetindex)
+          sheet.xpath("table:table").each_with_index do |table,tableindex|
+            t = s.create_or_open_table_at(tableindex)
+            table.xpath("table:table-row").each_with_index do |row,rowindex|
+              row = row.xpath("table:table-cell").collect do |cell|
+                c = Workbook::Cell.new()
+                
+                valuetype = cell.xpath('@office:value-type').to_s
+                value = cell.xpath("text:p//text()").to_s
+                value = value == "" ? nil : value
+                case valuetype
+                when 'float'
+                  value = cell.xpath("@office:value").to_s.to_f
+                when 'integer'
+                  value = cell.xpath("@office:value").to_s.to_i
+                when 'date'
+                  value = DateTime.parse(cell.xpath("@office:date-value").to_s)
+                end                
+                c.value = value 
+              end
+              t << Workbook::Row.new(row)
+            end
+          end
         end
-        return wb
       end
     end
   end
