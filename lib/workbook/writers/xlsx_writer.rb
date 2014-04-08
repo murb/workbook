64a738e8a4c53fc6c3bdb14dfd282dd1cb2eb080
Maarten Brouwers <github.com@murb.nl>
2014-04-08 16:15:17 +0200

    starting xlsx writing (unstable)

---
+++ lib/workbook/writers/xlsx_writer.rb
@@ -0,0 +1,132 @@
+# -*- encoding : utf-8 -*-
+require 'rubyXL'
+require 'workbook/readers/xls_shared'
+
+module Workbook
+  module Writers
+    module XlsxWriter
+
+      # Generates an Spreadsheet (from the spreadsheet gem) in order to build an XlS
+      #
+      # @param [Hash] options A hash with options (unused so far)
+      # @return [Spreadsheet] A Spreadsheet object, ready for writing or more lower level operations
+      def to_xlsx options={}
+        book = init_xlsx_spreadsheet_template
+        book.worksheets.pop(book.worksheets.count - self.count) if book.worksheets and book.worksheets.count > self.count
+        self.each_with_index do |s,si|
+          xls_sheet = xls_sheet(si)
+          xls_sheet.name = s.name
+
+          s.table.each_with_index do |r, ri|
+            xls_sheet.row(ri).height= r.format[:height] if r.format
+            r.each_with_index do |c, ci|
+              if c
+                if r.first?
+                  xls_sheet.columns[ci] ||= Spreadsheet::Column.new(ci,nil)
+                  xls_sheet.columns[ci].width= c.format[:width]
+                end
+                xls_sheet.row(ri)[ci] = c.value
+                xls_sheet.row(ri).set_format(ci, format_to_xls_format(c.format))
+              end
+            end
+          end
+          (xls_sheet.last_row_index + 1 - s.table.count).times do |time|
+            row_to_remove = s.table.count+time
+            xls_sheet.row(row_to_remove).each_with_index do |c, ci|
+              xls_sheet.row(row_to_remove)[ci]=nil
+            end
+
+            xls_sheet.delete_row(row_to_remove)
+            xls_sheet.row_updated(row_to_remove, xls_sheet.row(row_to_remove))
+          end
+          xls_sheet.updated_from(s.table.count)
+          xls_sheet.dimensions
+
+        end
+        book
+      end
+
+      # # Generates an Spreadsheet (from the spreadsheet gem) in order to build an XlS
+      # #
+      # # @param [Workbook::Format, Hash] f A Workbook::Format or hash with format-options (:font_weight, :rotation, :background_color, :number_format, :text_direction, :color, :font_family)
+      # # @return [Spreadsheet::Format] A Spreadsheet format-object, ready for writing or more lower level operations
+      # def format_to_xlsx_format f
+      #   xlsfmt = nil
+      #   unless f.is_a? Workbook::Format
+      #     f = Workbook::Format.new f
+      #   end
+      #   xlsfmt = f.return_raw_for Spreadsheet::Format
+      #   unless xlsfmt
+      #     xlsfmt=Spreadsheet::Format.new :weight=>f[:font_weight]
+      #     xlsfmt.rotation = f[:rotation] if f[:rotation]
+      #     xlsfmt.pattern_fg_color = html_color_to_xls_color(f[:background_color]) if html_color_to_xls_color(f[:background_color])
+      #     xlsfmt.pattern = 1 if html_color_to_xls_color(f[:background_color])
+      #     xlsfmt.number_format = strftime_to_ms_format(f[:number_format]) if f[:number_format]
+      #     xlsfmt.text_direction = f[:text_direction] if f[:text_direction]
+      #     xlsfmt.font.name = f[:font_family].split.first if f[:font_family]
+      #     xlsfmt.font.family = parse_font_family(f) if f[:font_family]
+      #     color = html_color_to_xls_color(f[:color])
+      #     xlsfmt.font.color = color if color
+      #     f.add_raw xlsfmt
+      #   end
+      #   return xlsfmt
+      # end
+      #
+      # # Parses right font-family name
+      # #
+      # # @param [Workbook::Format, hash] format to parse
+      # def parse_font_family(format)
+      #   font = format[:font_family].to_s.split.last
+      #   valid_values = [:none,:roman,:swiss,:modern,:script,:decorative]
+      #   if valid_values.include?(font)
+      #     return font
+      #   elsif valid_values.include?(font.to_s.downcase.to_sym)
+      #     return font.to_s.downcase.to_sym
+      #   else
+      #     font = font.to_s.downcase.strip
+      #     translation = {
+      #       "arial"=>:swiss,
+      #       "times"=>:roman,
+      #       "times new roman"=>:roman
+      #     }
+      #     tfont = translation[font]
+      #     return tfont ? tfont : :none
+      #   end
+      # end
+      #
+
+      # Write the current workbook to Microsoft Excel format (using the spreadsheet gem)
+      #
+      # @param [String] filename
+      # @param [Hash] options   see #to_xls
+      def write_to_xlsx filename="#{title}.xlsx", options={}
+        if to_xlsx(options).write(filename)
+          return filename
+        end
+      end
+
+      def xlsx_sheet a
+        if xlsx_template.worksheet[a]
+          return xlsx_template.worksheet[a]
+        else
+          xlsx_template.create_worksheet
+          self.xls_sheet a
+        end
+      end
+
+      def xlsx_template
+        return template.raws[RubyXL::Workbook]
+      end
+
+      def init_xlsx_spreadsheet_template
+        if self.xlsx_template.is_a? RubyXL::Workbook
+          return self.xlsx_template
+        else
+          t = RubyXL::Workbook.new
+          template.add_raw t
+          return t
+        end
+      end
+    end
+  end
+end
---
+++ test/test_writers_xlsx_writer.rb
@@ -0,0 +1,140 @@
+# -*- encoding : utf-8 -*-
+require File.join(File.dirname(__FILE__), 'helper')
+
+module Writers
+  class TestXlsWriter < Test::Unit::TestCase
+    def test_to_xls
+      b = Workbook::Book.new [['a','b','c'],[1,2,3],[3,2,3]]
+      raw = Spreadsheet.open File.join(File.dirname(__FILE__), 'artifacts/simple_sheet.xls')
+      t = Workbook::Template.new
+      t.add_raw raw
+      b.template = t
+      assert_equal(true, b.to_xls.is_a?(Spreadsheet::Workbook))
+
+      assert_equal('untitled document.xls', b.write_to_xls)
+    end
+
+    def test_roundtrip
+      b = Workbook::Book.open File.join(File.dirname(__FILE__), 'artifacts/simple_sheet.xls')
+      assert_equal(3.85546875,b.sheet.table.first[:a].format[:width])
+      filename = b.write_to_xls
+      b = Workbook::Book.open filename
+      assert_equal(3.85546875,b.sheet.table.first[:a].format[:width])
+    end
+    def test_delete_row
+      b = Workbook::Book.open File.join(File.dirname(__FILE__), 'artifacts/simple_sheet.xls')
+      # a  b  c  d  e
+      # 14  90589  a  19 apr 12  23 apr 12
+      # 15  90588  b  15 nov 11  16 jul 12
+      # 25  90463  c  15 nov 11  17 nov 11
+      # 33  90490  d  13 mrt 12  15 mrt 12
+      t = b.sheet.table
+      assert_equal(33, t.last.first.value)
+      t.delete_at(4) #delete last row
+      filename = b.write_to_xls
+      b = Workbook::Book.open filename
+      t = b.sheet.table
+      # puts t.to_csv
+      #TODO: NOT true delete... need to work on this...
+      assert_equal(25, t[3].first.value)
+      assert_equal(nil, t[4].first.value)
+      assert_equal(nil, t[4].last.value)
+    end
+    def test_pop_row
+      b = Workbook::Book.open File.join(File.dirname(__FILE__), 'artifacts/simple_sheet.xls')
+      # a  b  c  d  e
+      # 14  90589  a  19 apr 12  23 apr 12
+      # 15  90588  b  15 nov 11  16 jul 12
+      # 25  90463  c  15 nov 11  17 nov 11
+      # 33  90490  d  13 mrt 12  15 mrt 12
+      t = b.sheet.table
+      assert_equal(33, t.last.first.value)
+      t.pop(2) #delete last two row
+      # puts t.to_csv
+      filename = b.write_to_xls
+      b = Workbook::Book.open filename
+      t = b.sheet.table
+      # puts t.to_csv
+      #TODO: NOT true delete... need to work on this...
+      assert_equal(nil, t[3].first.value)
+      assert_equal(nil, t[4].first.value)
+      assert_equal(nil, t[4].last.value)
+      assert_equal(15, t[2].first.value)
+      assert_equal(nil, t.last.first.value)
+
+    end
+    def test_pop_bigtable
+      b = Workbook::Book.open File.join(File.dirname(__FILE__), 'artifacts/bigtable.xls')
+      # a  b  c  d  e
+      # 14  90589  a  19 apr 12  23 apr 12
+      # 15  90588  b  15 nov 11  16 jul 12
+      # 25  90463  c  15 nov 11  17 nov 11
+      # 33  90490  d  13 mrt 12  15 mrt 12
+      t = b.sheet.table
+      assert_equal(574, t.count)
+      t.pop(300) #delete last two row
+      assert_equal(274, t.trim.count)
+      filename = b.write_to_xls
+      b = Workbook::Book.open filename
+      t = b.sheet.table
+      assert_equal(274, t.trim.count)
+
+
+    end
+    def test_cloning_roundtrip
+      b = Workbook::Book.open File.join(File.dirname(__FILE__), 'artifacts/book_with_tabs_and_colours.xls')
+      b.sheet.table << b.sheet.table[2]
+      assert_equal(90588,b.sheet.table[5][:b].value)
+      assert_equal("#FFFF00",b.sheet.table[5][:c].format[:background_color])
+      filename = b.write_to_xls
+      b = Workbook::Book.open filename
+      assert_equal(90588,b.sheet.table[5][:b].value)
+      assert_equal("#FFFF00",b.sheet.table[5][:c].format[:background_color])
+    end
+    def test_parse_font_family
+      b = Workbook::Book.new
+      assert_equal(:none,b.parse_font_family({:font_family=>"asdfsdf"}))
+      assert_equal(:swiss,b.parse_font_family({:font_family=>"ArIAL"}))
+      assert_equal(:swiss,b.parse_font_family({:font_family=>:swiss}))
+      assert_equal(:roman,b.parse_font_family({:font_family=>"Times"}))
+      assert_equal(:roman,b.parse_font_family({:font_family=>"roman"}))
+    end
+
+    def test_init_spreadsheet_template
+      b = Workbook::Book.new
+      b.init_spreadsheet_template
+      assert_equal(Spreadsheet::Workbook,b.xls_template.class)
+    end
+
+    def test_xls_sheet
+      b = Workbook::Book.new
+      b.init_spreadsheet_template
+      assert_equal(Spreadsheet::Worksheet,b.xls_sheet(100).class)
+    end
+    def test_strftime_to_ms_format_nil
+      assert_equal(nil, Workbook::Book.new.strftime_to_ms_format(nil))
+    end
+    def test_xls_sheet_writer
+      b = Workbook::Book.new
+      b << Workbook::Sheet.new
+      b << Workbook::Sheet.new
+      b[0].name = "A"
+      b[1].name = "B"
+      b[2].name = "C"
+      assert_equal(["A","B","C"], b.collect{|a| a.name})
+      filename = b.write_to_xls
+      b = Workbook::Book.open filename
+      assert_equal(["A","B","C"], b.collect{|a| a.name})
+    end
+    def test_removal_of_sheets_in_excel_when_using_template
+      b = Workbook::Book.open File.join(File.dirname(__FILE__), 'artifacts/simple_sheet_many_sheets.xls')
+      assert_equal(10, b.count)
+      b.pop(4)
+      assert_equal(6, b.count)
+      filename = b.write_to_xls
+      b = Workbook::Book.open filename
+      assert_equal(6, b.count)
+
+    end
+  end
+end
