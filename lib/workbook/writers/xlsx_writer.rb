301a80672cd446069bdea95240469f64b4e9d6c5
Maarten Brouwers <github.com@murb.nl>
2015-01-20 16:36:09 +0100

    streaming support

--- lib/workbook/writers/xlsx_writer.rb
+++ lib/workbook/writers/xlsx_writer.rb
@@ -6,7 +6,7 @@ module Workbook
   module Writers
     module XlsxWriter
 
-      # Generates an Spreadsheet (from the spreadsheet gem) in order to build an XlS
+      # Generates an RubyXL doc, ready for export to XLSX
       #
       # @param [Hash] options A hash with options (unused so far)
       # @return [Spreadsheet] A Spreadsheet object, ready for writing or more lower level operations
@@ -37,59 +37,18 @@ module Workbook
         book
       end
 
-      # # Generates an Spreadsheet (from the spreadsheet gem) in order to build an XlS
-      # #
-      # # @param [Workbook::Format, Hash] f A Workbook::Format or hash with format-options (:font_weight, :rotation, :background_color, :number_format, :text_direction, :color, :font_family)
-      # # @return [Spreadsheet::Format] A Spreadsheet format-object, ready for writing or more lower level operations
-      # def format_to_xlsx_format f
-      #   xlsfmt = nil
-      #   unless f.is_a? Workbook::Format
-      #     f = Workbook::Format.new f
-      #   end
-      #   xlsfmt = f.return_raw_for Spreadsheet::Format
-      #   unless xlsfmt
-      #     xlsfmt=Spreadsheet::Format.new :weight=>f[:font_weight]
-      #     xlsfmt.rotation = f[:rotation] if f[:rotation]
-      #     xlsfmt.pattern_fg_color = html_color_to_xls_color(f[:background_color]) if html_color_to_xls_color(f[:background_color])
-      #     xlsfmt.pattern = 1 if html_color_to_xls_color(f[:background_color])
-      #     xlsfmt.number_format = strftime_to_ms_format(f[:number_format]) if f[:number_format]
-      #     xlsfmt.text_direction = f[:text_direction] if f[:text_direction]
-      #     xlsfmt.font.name = f[:font_family].split.first if f[:font_family]
-      #     xlsfmt.font.family = parse_font_family(f) if f[:font_family]
-      #     color = html_color_to_xls_color(f[:color])
-      #     xlsfmt.font.color = color if color
-      #     f.add_raw xlsfmt
-      #   end
-      #   return xlsfmt
-      # end
+      # Generates an RubyXL doc, ready for export to XLSX
       #
-      # # Parses right font-family name
-      # #
-      # # @param [Workbook::Format, hash] format to parse
-      # def parse_font_family(format)
-      #   font = format[:font_family].to_s.split.last
-      #   valid_values = [:none,:roman,:swiss,:modern,:script,:decorative]
-      #   if valid_values.include?(font)
-      #     return font
-      #   elsif valid_values.include?(font.to_s.downcase.to_sym)
-      #     return font.to_s.downcase.to_sym
-      #   else
-      #     font = font.to_s.downcase.strip
-      #     translation = {
-      #       "arial"=>:swiss,
-      #       "times"=>:roman,
-      #       "times new roman"=>:roman
-      #     }
-      #     tfont = translation[font]
-      #     return tfont ? tfont : :none
-      #   end
-      # end
-      #
+      # @param [Hash] options A hash with options (unused so far)
+      # @return [String] A string, ready for streaming, e.g. `send_data workbook.stream_xlsx`
+      def stream_xlsx options = {}
+        to_xlsx(options).stream.string
+      end
 
-      # Write the current workbook to Microsoft Excel format (using the spreadsheet gem)
+      # Write the current workbook to Microsoft Excel's new format (using the RubyXL gem)
       #
       # @param [String] filename
-      # @param [Hash] options   see #to_xls
+      # @param [Hash] options   see #to_xlsx
       def write_to_xlsx filename="#{title}.xlsx", options={}
         if to_xlsx(options).write(filename)
           return filename
