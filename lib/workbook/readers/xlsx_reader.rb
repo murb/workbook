6fd4333b7d03c80e8c2d5a578ceef2813426349f
Maarten Brouwers <github.com@murb.nl>
2016-09-23 16:12:23 +0200

    implemented new xlsx reader

--- lib/workbook/readers/xlsx_reader.rb
+++ lib/workbook/readers/xlsx_reader.rb
@@ -1,37 +1,176 @@
 # -*- encoding : utf-8 -*-
-require 'roo'
 require 'workbook/readers/xls_shared'
-
 
 module Workbook
   module Readers
     module XlsxReader
       include Workbook::Readers::XlsShared
 
-      # Load method for .xlsm files, an office open file format, hence compatible with .xlsx (it emphasizes that it contains macros)
-      #
-      # @param [String, File] file_obj   a string with a reference to the file to be written to
       def load_xlsm file_obj, options={}
         self.load_xlsx file_obj, options
       end
       def load_xlsx file_obj, options={}
-        # file_obj = file_obj.match(/^\/(.*)/) ? file_obj : "./#{file_obj}"
-        # p "opening #{file_obj}"
-        sp = Roo::Excelx.new(file_obj)
-        template.add_raw sp, raw_object_class: Roo::Spreadsheet
-        parse_xlsx sp
+        file_obj = file_obj.path if file_obj.is_a? File
+        sheets = {}
+        shared_string_file = ""
+        styles = ""
+        workbook = ""
+        workbook_rels = ""
+        Zip::File.open(file_obj) do |zipfile|
+          zipfile.entries.each do |file|
+            if file.name.match(/^xl\/worksheets\/(.*)\.xml$/)
+              sheets[file.name.sub(/^xl\//,'')] = zipfile.read(file.name)
+            elsif file.name.match(/xl\/sharedStrings.xml$/)
+              shared_string_file = zipfile.read(file.name)
+            elsif file.name.match(/xl\/workbook.xml$/)
+              workbook = zipfile.read(file.name)
+            elsif file.name.match(/xl\/_rels\/workbook.xml.rels$/)
+              workbook_rels = zipfile.read(file.name)
+            elsif file.name.match(/xl\/styles.xml$/)
+              styles = zipfile.read(file.name)
+            end
+            # content = zipfile.read(file.name) if file.name == "content.xml"
+          end
+        end
+
+        parse_xlsx_styles(styles)
+
+
+        relation_file = {}
+        Nokogiri::XML(workbook_rels).css("Relationships Relationship").each do |relship|
+          relation_file[relship.attr("Id")]=relship.attr("Target")
+        end
+
+        @shared_strings = parse_shared_string_file(shared_string_file)
+
+        Nokogiri::XML(workbook).css("sheets sheet").each do |sheet|
+          name = sheet.attr("name")
+          filename = relation_file[sheet.attr("r:id")]
+          state = sheet.attr("state")
+          if state != "hidden"
+            sheet = sheets[filename]
+            self.push parse_xlsx_sheet(sheet)
+            self.last.name = name
+          end
+        end
+
+        @shared_strings = nil
+        self.each do |sheet|
+          sheet.each do |table|
+            table.trim!
+          end
+        end
       end
 
-      def parse_xlsx xlsx_spreadsheet=template.raws[Roo::Spreadsheet], options={}
-        options = {:additional_type_parsing=>false}.merge options
-        sheet_index = 0
-        xlsx_spreadsheet.each_with_pagename do |sheet_name, sheet|
-          s = create_or_open_sheet_at(sheet_index)
-          sheet.each_with_index do |row, rowi|
-            s.table << row
+      def parse_xlsx_styles(styles)
+        styles = Nokogiri::XML(styles)
+
+        fonts = parse_xlsx_fonts styles
+        backgrounds = extract_xlsx_backgrounds styles
+        customNumberFormats = extract_xlsx_number_formats styles
+
+
+        styles.css("cellXfs xf").each do |cellformat|
+          hash = {}
+          # <xf numFmtId="0" fontId="1" fillId="2" borderId="0" xfId="0" applyFont="1" applyFill="1" applyAlignment="1">
+          background_hash = backgrounds[cellformat.attr("applyFill").to_i]
+          hash.merge!(background_hash) if background_hash
+
+          font_hash = fonts[cellformat.attr("applyFill").to_i]
+          hash.merge!(font_hash) if font_hash
+
+          id = (cellformat.attr("numFmtId")).to_i
+          if id >= 164
+            hash[:numberformat] = customNumberFormats[id]
+          else
+            hash[:numberformat] = ms_formatting_to_strftime(id)
           end
-          sheet_index += 1
+          self.template.add_format Workbook::Format.new(hash)
         end
+      end
+
+      # Extracts fonts descriptors from styles.xml
+      def parse_xlsx_fonts styles
+        styles.css("fonts font").collect do |font|
+          hash = {}
+          hash[:font_family] = font.css("name").first.attr("val") if font.css("name")
+          hash[:font_size] = font.css("sz").first.attr("val").to_i if font.css("name")
+          hash
+        end
+      end
+
+      # Extracts number formats from styles.xml
+      def extract_xlsx_number_formats styles
+        hash = {}
+        styles.css("numFmts numFmt").each do |fmt|
+          format_id = fmt.attr("numFmtId").to_i
+          parsed_format_string = ms_formatting_to_strftime(fmt.attr("formatCode"))
+          hash[format_id] = parsed_format_string
+        end
+        hash
+      end
+
+      def extract_xlsx_backgrounds styles
+        styles.css("fills fill").collect do |fill|
+          hash = {}
+          patternFill = fill.css("patternFill").first
+          # TODO: convert to html-hex
+          hash[:background] = patternFill.attr("patternType") if patternFill
+          hash
+        end
+      end
+
+      def parse_shared_string_file file
+        Nokogiri::XML(file).css("sst si t").collect{|a| a.text}
+      end
+      def parse_xlsx_sheet sheet_xml
+        sheet = Workbook::Sheet.new
+        table = sheet.table
+
+        noko_xml = Nokogiri::XML(sheet_xml)
+
+        rows = noko_xml.css('sheetData row').collect{|row| parse_xlsx_row(row)}
+        rows.each do |row|
+          table << row
+        end
+
+        columns = noko_xml.css('cols col').collect{|col| parse_xlsx_column(col) }
+        table.columns = columns
+
+        sheet
+      end
+
+      def parse_xlsx_column column
+        col = Workbook::Column.new()
+        col.width = column.attr("width").to_f
+        col
+      end
+      def parse_xlsx_row row
+        cells = row.css('c').collect{|a| parse_xlsx_cell(a)}
+        row = Workbook::Row.new(cells)
+      end
+      def parse_xlsx_cell cell
+        # style_id = cell.attr('s')
+        # p cell
+        type = cell.attr('t')
+        formatIndex = cell.attr('s').to_i
+        value = cell.text
+        fmt = template.formats[formatIndex]
+        # puts type
+        if type == "n" or type == nil
+          if fmt.derived_type == :date
+            value = xls_number_to_date(value)
+          elsif fmt.derived_type == :time
+            value = xls_number_to_time(value)
+          elsif type == "n"
+            value = value.match(/\./) ? value.to_f : value.to_i
+          end
+        elsif type == "s"
+          value = @shared_strings[value.to_i]
+        end
+        Workbook::Cell.new(value, format: fmt)
+      end
+      def parse_xlsx
       end
     end
   end
--- test/test_readers_xlsx_reader.rb
+++ test/test_readers_xlsx_reader.rb
@@ -1,11 +1,12 @@
 # -*- encoding : utf-8 -*-
 require File.join(File.dirname(__FILE__), 'helper')
 module Readers
-  class TestXlsxWriter < Minitest::Test
+  class TestXlsxReader < Minitest::Test
     def test_xlsx_open
       w = Workbook::Book.new
       w.open File.join(File.dirname(__FILE__), 'artifacts/book_with_tabs_and_colours.xlsx')
       assert_equal([:a, :b, :c, :d, :e],w.sheet.table.header.to_symbols)
+      assert_equal([:anders,:dit],w[1].table.header.to_symbols)
       assert_equal(90588,w.sheet.table[2][:b].value)
       assert_equal(DateTime.new(2011,11,15),w.sheet.table[3][:d].value)
       # assert_equal("#CCFFCC",w.sheet.table[3][:c].format[:background_color]) #colour compatibility turned off for now...
@@ -16,9 +17,7 @@ module Readers
     def test_open_native_xlsx
       w = Workbook::Book.new
       w.open File.join(File.dirname(__FILE__), 'artifacts/native_xlsx.xlsx')
-
-      assert_equal([:datum_gemeld, :adm_gereed, :callnr],w.sheet.table.header.to_symbols)
-
+      assert_equal([:datum_gemeld, :adm_gereed, :callnr],w.last.table.header.to_symbols)
       assert_equal("Callnr.",w.sheet.table[0][:callnr].value)
       assert_equal("2475617.00",w.sheet.table[3][:callnr].value)
       assert_equal("2012-12-03T12:30:00+00:00",w.sheet.table[7][:datum_gemeld].value.to_s)
@@ -31,8 +30,12 @@ module Readers
     end
     def test_open_integer_xlsx
       w = Workbook::Book.open File.join(File.dirname(__FILE__), 'artifacts/integer_test.xlsx')
-
       assert_equal("2",w.sheet.table[1][1].value.to_s)
+      assert_equal(2,w.sheet.table[1][1].value)
+    end
+    def test_bit_table_xlsx
+      b = Workbook::Book.open File.join(File.dirname(__FILE__), 'artifacts/bigtable.xlsx')
+      assert_equal(553, b.sheet.table.count)
     end
   end
 end
